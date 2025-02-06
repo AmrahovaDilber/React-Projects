b7ac138ce8cdd178e9839f109be42a1b29ec7267
AmrahovaDilber <amrahovad911@gmail.com>
2025-02-06 22:03:03 +0400

    Added Hide-Show Details functionality

--- Tabs/src/components/Content.tsx
+++ Tabs/src/components/Content.tsx
@@ -1,20 +1,26 @@
+import { useState } from "react";
 import { islamicReminders } from "../constant/data";
 
 export const Content: React.FC<{ activeTab: number }> = ({ activeTab }) => {
-    return (
-      <div className="content-container">
-        <div>
-          <h3>{islamicReminders[activeTab - 1].title}</h3>
-          <p>{islamicReminders[activeTab - 1].details}</p>
-          <div className="content-buttons">
-            <button className="hide-btn">Hide Details</button>
-            <div className="heart-section">
-              <p>0 heart</p>
-              <button className="heart-btn">+</button>
-            </div>
+  const [showDetails, setShowDetails] = useState<boolean>(true);
+  return (
+    <div className="content-container">
+      <div>
+        <h3>{islamicReminders[activeTab - 1].title}</h3>
+        {showDetails && <p>{islamicReminders[activeTab - 1].details}</p>}
+        <div className="content-buttons">
+          <button
+            className="hide-btn"
+            onClick={() => setShowDetails(!showDetails)}
+          >
+          {showDetails?"Hide Details":"Show Details"}
+          </button>
+          <div className="heart-section">
+            <p>0 heart</p>
+            <button className="heart-btn">+</button>
           </div>
         </div>
       </div>
-    );
-  };
-  
+    </div>
+  );
+};
